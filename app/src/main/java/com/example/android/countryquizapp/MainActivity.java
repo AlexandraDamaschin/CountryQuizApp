b23413ba19724de9356ce8def1451a897b8626f7
Alexandra Maria Damaschin <damaschin.alexandra.maria@gmail.com>
2018-03-12 02:29:49 +0000

    first question implemented

--- app/src/main/java/com/example/android/countryquizapp/MainActivity.java
+++ app/src/main/java/com/example/android/countryquizapp/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.android.countryquizapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioButton;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,40 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    //    wrong answer = no points
+    public void wrongAnswer(View view) {
+
+    }
+
+    //answer correct = points and next question
+    public void goToSecondQuestion(View view) {
+        // Is the button now checked?
+        boolean checked = ((RadioButton) view).isChecked();
+        // Check which radio button was clicked
+        switch (view.getId()) {
+            case R.id.q1_ans1:
+                if (checked) {
+                    Toast.makeText(this, getString(R.string.wrong_answer), Toast.LENGTH_SHORT).show();
+                    break;
+                }
+            case R.id.q1_ans2:
+                if (checked) {
+                    Toast.makeText(this, getString(R.string.wrong_answer), Toast.LENGTH_SHORT).show();
+                    break;
+                }
+            case R.id.q1_ans3:
+                if (checked) {
+                    Toast.makeText(this, getString(R.string.wrong_answer), Toast.LENGTH_SHORT).show();
+                    break;
+                }
+
+            case R.id.q1_ans_correct:
+                if (checked) {
+                    setContentView(R.layout.question_2);
+                    break;
+                }
+        }
     }
 }
