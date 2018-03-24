cccfa88b2f6aa19434e002535d75416a70ee433d
AlexandraDamaschin <damaschin.alexandra.maria@gmail.com>
2018-03-24 12:28:41 +0000

    method implemented

--- app/src/main/java/com/example/android/countryquizapp/MainActivity.java
+++ app/src/main/java/com/example/android/countryquizapp/MainActivity.java
@@ -17,6 +17,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
+    //start quiz
     public void startQuiz(View view) {
         setContentView(R.layout.question_1);
     }
@@ -27,8 +28,45 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.question_2);
     }
 
-    //answer correct = 1 point
+    //first question
+    //if answer is right increase score and go to second question
     public void goToSecondQuestion(View view) {
+        // Is the button now checked?
+        boolean checked = ((RadioButton) view).isChecked();
+        // Check which radio button was clicked
+        switch (view.getId()) {
+            //answer correct = 1 point
+            case R.id.q2_ans_correct:
+                if (checked) {
+                    setContentView(R.layout.question_3);
+                    score++;
+                    break;
+                }
+            case R.id.q2_ans1:
+                if (checked) {
+                    Toast.makeText(this, getString(R.string.wrong_answer), Toast.LENGTH_SHORT).show();
+                    setContentView(R.layout.question_2);
+                    break;
+                }
+            case R.id.q2_ans2:
+                if (checked) {
+                    Toast.makeText(this, getString(R.string.wrong_answer), Toast.LENGTH_SHORT).show();
+                    setContentView(R.layout.question_2);
+                    break;
+                }
+
+            case R.id.q2_ans3:
+                if (checked) {
+                    Toast.makeText(this, getString(R.string.wrong_answer), Toast.LENGTH_SHORT).show();
+                    setContentView(R.layout.question_2);
+                    break;
+                }
+        }
+    }
+
+    //second question
+    //if answer is right increase score and go to third question
+    public void goToThirdQuestion(View view) {
         // Is the button now checked?
         boolean checked = ((RadioButton) view).isChecked();
         // Check which radio button was clicked
@@ -52,6 +90,295 @@ public class MainActivity extends AppCompatActivity {
                     break;
                 }
 
+                //answer correct = 1 point
+            case R.id.q1_ans_correct:
+                if (checked) {
+                    setContentView(R.layout.question_2);
+                    score++;
+                    break;
+                }
+        }
+    }
+
+    //third question
+    //if answer right increase score and to to fourth question
+    public void goToFourthQuestion(View view) {
+        // Is the button now checked?
+        boolean checked = ((RadioButton) view).isChecked();
+        // Check which radio button was clicked
+        switch (view.getId()) {
+            case R.id.q1_ans1:
+                if (checked) {
+                    Toast.makeText(this, getString(R.string.wrong_answer), Toast.LENGTH_SHORT).show();
+                    setContentView(R.layout.question_2);
+                    break;
+                }
+            case R.id.q1_ans2:
+                if (checked) {
+                    Toast.makeText(this, getString(R.string.wrong_answer), Toast.LENGTH_SHORT).show();
+                    setContentView(R.layout.question_2);
+                    break;
+                }
+            case R.id.q1_ans3:
+                if (checked) {
+                    Toast.makeText(this, getString(R.string.wrong_answer), Toast.LENGTH_SHORT).show();
+                    setContentView(R.layout.question_2);
+                    break;
+                }
+
+                //answer correct = 1 point
+            case R.id.q1_ans_correct:
+                if (checked) {
+                    setContentView(R.layout.question_2);
+                    score++;
+                    break;
+                }
+        }
+    }
+
+    //fourth question
+    //if answer right increase score and to to fifth question
+    public void goToFifthQuestion(View view) {
+        // Is the button now checked?
+        boolean checked = ((RadioButton) view).isChecked();
+        // Check which radio button was clicked
+        switch (view.getId()) {
+            case R.id.q1_ans1:
+                if (checked) {
+                    Toast.makeText(this, getString(R.string.wrong_answer), Toast.LENGTH_SHORT).show();
+                    setContentView(R.layout.question_2);
+                    break;
+                }
+            case R.id.q1_ans2:
+                if (checked) {
+                    Toast.makeText(this, getString(R.string.wrong_answer), Toast.LENGTH_SHORT).show();
+                    setContentView(R.layout.question_2);
+                    break;
+                }
+            case R.id.q1_ans3:
+                if (checked) {
+                    Toast.makeText(this, getString(R.string.wrong_answer), Toast.LENGTH_SHORT).show();
+                    setContentView(R.layout.question_2);
+                    break;
+                }
+
+                //answer correct = 1 point
+            case R.id.q1_ans_correct:
+                if (checked) {
+                    setContentView(R.layout.question_2);
+                    score++;
+                    break;
+                }
+        }
+    }
+
+    //fifth question
+    //if answer right increase score and to to sixth question
+    public void goToSixthQuestion(View view) {
+        // Is the button now checked?
+        boolean checked = ((RadioButton) view).isChecked();
+        // Check which radio button was clicked
+        switch (view.getId()) {
+            case R.id.q1_ans1:
+                if (checked) {
+                    Toast.makeText(this, getString(R.string.wrong_answer), Toast.LENGTH_SHORT).show();
+                    setContentView(R.layout.question_2);
+                    break;
+                }
+            case R.id.q1_ans2:
+                if (checked) {
+                    Toast.makeText(this, getString(R.string.wrong_answer), Toast.LENGTH_SHORT).show();
+                    setContentView(R.layout.question_2);
+                    break;
+                }
+            case R.id.q1_ans3:
+                if (checked) {
+                    Toast.makeText(this, getString(R.string.wrong_answer), Toast.LENGTH_SHORT).show();
+                    setContentView(R.layout.question_2);
+                    break;
+                }
+
+                //answer correct = 1 point
+            case R.id.q1_ans_correct:
+                if (checked) {
+                    setContentView(R.layout.question_2);
+                    score++;
+                    break;
+                }
+        }
+    }
+
+    //sixth question
+    //if answer right increase score and to to seven question
+    public void goToSevenQuestion(View view) {
+        // Is the button now checked?
+        boolean checked = ((RadioButton) view).isChecked();
+        // Check which radio button was clicked
+        switch (view.getId()) {
+            case R.id.q1_ans1:
+                if (checked) {
+                    Toast.makeText(this, getString(R.string.wrong_answer), Toast.LENGTH_SHORT).show();
+                    setContentView(R.layout.question_2);
+                    break;
+                }
+            case R.id.q1_ans2:
+                if (checked) {
+                    Toast.makeText(this, getString(R.string.wrong_answer), Toast.LENGTH_SHORT).show();
+                    setContentView(R.layout.question_2);
+                    break;
+                }
+            case R.id.q1_ans3:
+                if (checked) {
+                    Toast.makeText(this, getString(R.string.wrong_answer), Toast.LENGTH_SHORT).show();
+                    setContentView(R.layout.question_2);
+                    break;
+                }
+
+                //answer correct = 1 point
+            case R.id.q1_ans_correct:
+                if (checked) {
+                    setContentView(R.layout.question_2);
+                    score++;
+                    break;
+                }
+        }
+    }
+
+    //seven question
+    //if answer right increase score and to to eight question
+    public void goToEightQuestion(View view) {
+        // Is the button now checked?
+        boolean checked = ((RadioButton) view).isChecked();
+        // Check which radio button was clicked
+        switch (view.getId()) {
+            case R.id.q1_ans1:
+                if (checked) {
+                    Toast.makeText(this, getString(R.string.wrong_answer), Toast.LENGTH_SHORT).show();
+                    setContentView(R.layout.question_2);
+                    break;
+                }
+            case R.id.q1_ans2:
+                if (checked) {
+                    Toast.makeText(this, getString(R.string.wrong_answer), Toast.LENGTH_SHORT).show();
+                    setContentView(R.layout.question_2);
+                    break;
+                }
+            case R.id.q1_ans3:
+                if (checked) {
+                    Toast.makeText(this, getString(R.string.wrong_answer), Toast.LENGTH_SHORT).show();
+                    setContentView(R.layout.question_2);
+                    break;
+                }
+
+                //answer correct = 1 point
+            case R.id.q1_ans_correct:
+                if (checked) {
+                    setContentView(R.layout.question_2);
+                    score++;
+                    break;
+                }
+        }
+    }
+
+    //eight question
+    //if answer right increase score and to to nine question
+    public void goToNineQuestion(View view) {
+        // Is the button now checked?
+        boolean checked = ((RadioButton) view).isChecked();
+        // Check which radio button was clicked
+        switch (view.getId()) {
+            case R.id.q1_ans1:
+                if (checked) {
+                    Toast.makeText(this, getString(R.string.wrong_answer), Toast.LENGTH_SHORT).show();
+                    setContentView(R.layout.question_2);
+                    break;
+                }
+            case R.id.q1_ans2:
+                if (checked) {
+                    Toast.makeText(this, getString(R.string.wrong_answer), Toast.LENGTH_SHORT).show();
+                    setContentView(R.layout.question_2);
+                    break;
+                }
+            case R.id.q1_ans3:
+                if (checked) {
+                    Toast.makeText(this, getString(R.string.wrong_answer), Toast.LENGTH_SHORT).show();
+                    setContentView(R.layout.question_2);
+                    break;
+                }
+
+                //answer correct = 1 point
+            case R.id.q1_ans_correct:
+                if (checked) {
+                    setContentView(R.layout.question_2);
+                    score++;
+                    break;
+                }
+        }
+    }
+
+    //nine question
+    //if answer right increase score and to to ten question
+    public void goToTenQuestion(View view) {
+        // Is the button now checked?
+        boolean checked = ((RadioButton) view).isChecked();
+        // Check which radio button was clicked
+        switch (view.getId()) {
+            case R.id.q1_ans1:
+                if (checked) {
+                    Toast.makeText(this, getString(R.string.wrong_answer), Toast.LENGTH_SHORT).show();
+                    setContentView(R.layout.question_2);
+                    break;
+                }
+            case R.id.q1_ans2:
+                if (checked) {
+                    Toast.makeText(this, getString(R.string.wrong_answer), Toast.LENGTH_SHORT).show();
+                    setContentView(R.layout.question_2);
+                    break;
+                }
+            case R.id.q1_ans3:
+                if (checked) {
+                    Toast.makeText(this, getString(R.string.wrong_answer), Toast.LENGTH_SHORT).show();
+                    setContentView(R.layout.question_2);
+                    break;
+                }
+
+                //answer correct = 1 point
+            case R.id.q1_ans_correct:
+                if (checked) {
+                    setContentView(R.layout.question_2);
+                    score++;
+                    break;
+                }
+        }
+    }
+
+    //ten question
+    //if answer right increase score and to to seven question
+    public void goToScore(View view) {
+        // Is the button now checked?
+        boolean checked = ((RadioButton) view).isChecked();
+        // Check which radio button was clicked
+        switch (view.getId()) {
+            case R.id.q1_ans1:
+                if (checked) {
+                    Toast.makeText(this, getString(R.string.wrong_answer), Toast.LENGTH_SHORT).show();
+                    setContentView(R.layout.question_2);
+                    break;
+                }
+            case R.id.q1_ans2:
+                if (checked) {
+                    Toast.makeText(this, getString(R.string.wrong_answer), Toast.LENGTH_SHORT).show();
+                    setContentView(R.layout.question_2);
+                    break;
+                }
+            case R.id.q1_ans3:
+                if (checked) {
+                    Toast.makeText(this, getString(R.string.wrong_answer), Toast.LENGTH_SHORT).show();
+                    setContentView(R.layout.question_2);
+                    break;
+                }
+
+                //answer correct = 1 point
             case R.id.q1_ans_correct:
                 if (checked) {
                     setContentView(R.layout.question_2);
